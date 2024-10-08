de0bccc882d8caaef62fa50bdffaf9482703a004
Joshua <Joshino784@gmail.com>
2024-10-08 13:00:37 +0100

    lab2 finished

--- create_db.sql
+++ create_db.sql
@@ -10,3 +10,6 @@ CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price D
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON bettys_books.* TO ' bettys_books_app'@'localhost';
+
+#Create user database
+CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT,username VARCHAR(50),firstname VARCHAR(50),lastname VARCHAR(50),email VARCHAR(50),hashedpassword TEXT,PRIMARY KEY(id));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const bcrypt = require('bcrypt')
+
+
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')                                                               
@@ -8,7 +11,69 @@ router.get('/register', function (req, res, next) {
 
 router.post('/registered', function (req, res, next) {
     // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
+    const saltRounds = 10
+    const plainPassword = req.body.password
+
+    //Store hashed password in database
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        let sqlquery = "INSERT INTO users (username, firstname, lastname, email, hashedPassword) VALUES (?,?,?,?,?)"
+        // execute sql query
+        let newuser = [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword]
+        db.query(sqlquery, newuser, (err, result) => {
+            if (err) {
+                next(err)
+            }
+            else
+            result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email
+            result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword
+
+            res.send(result)                                                                           
+        })
+    })
+    
+})
+
+router.get('/list', function (req, res, next) {
+    let sqlquery = "SELECT * FROM users"
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("userlist.ejs", {users:result})
+     })
+})
+
+router.get('/login', function (req, res, next) {
+    res.render('login.ejs')                                                               
+})   
+
+router.post('/loggedin', function (req, res, next) {
+    let sqlquery = "SELECT hashedpassword from users where username=(?)"
+    const name = req.body.username
+    db.query(sqlquery, name, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        if (result.length === 0) {
+            res.send("Incorrect username")
+        }
+        const hashedPassword = result[0].hashedpassword      
+        bcrypt.compare(req.body.password, hashedPassword, function(err, result) {
+            if (err) {
+              next(err)
+            }
+            else if (result == true) {
+              // TODO: Send message
+              res.send("Sucessfully logged in " + name)
+            }
+            else {
+              // TODO: Send message
+              res.send("Incorrect Password")
+            }
+          })
+    }) 
+    
+  
 })
 
 // Export the router object so index.js can access it
