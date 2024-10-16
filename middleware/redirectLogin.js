466d1821d798670c747c7f37e096187f1c0ae5b3
Joshua <Joshino784@gmail.com>
2024-10-16 14:30:27 +0100

    updated the redirect login middleware

--- middleware/redirectLogin.js
+++ middleware/redirectLogin.js
@@ -1,7 +1,6 @@
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId) {
-        console.log(req.baseUrl)
-        res.redirect('/users/login'); // Absolute path to users/login
+        res.redirect('../users/login'); // Absolute path to users/login
     } else {
       next(); // move to the next middleware function
     }
