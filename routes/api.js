3494318f0ac380ecbd2f92c1a6ed357ff3605bcc
Joshua <Joshino784@gmail.com>
2024-10-29 15:51:44 +0000

    saving, continue later

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,13 @@ const request = require("request");
 
 router.get('/books', function (req, res, next) {
 
+    let searchTerm = req.query.search_term || '%'
+    console.log(searchTerm)
     // Query database to get all the books
-    let sqlquery = "SELECT * FROM books"
+    let sqlquery = "SELECT * FROM books where name LIKE ? "
 
     // Execute the sql query
-    db.query(sqlquery, (err, result) => {
+    db.query(sqlquery, [searchTerm], (err, result) => {
         // Return results as a JSON object
         if (err) {
             res.json(err)
@@ -24,14 +26,15 @@ router.get('/books', function (req, res, next) {
 })
 
 router.get('/myfriendsbooks', function(req, res, next) {
-    let url = 'https://www.doc.gold.ac.uk/usr/109/api/books';
+    let url = 'https://www.doc.gold.ac.uk/usr/108/api/books';
 
     request(url, function (err, response, body) {
         if (err) {
           next(err);
         } else {
             const books = JSON.parse(body)
-            res.json(books)
+            console.log(books)
+            res.render("myfriendsbooks", {books})
         }
       });
 
